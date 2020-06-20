0a2a77f588ebb65a500489544dfb69d28fab8e0c
GLinh1210 <1654050058linh@ou.edu.vn>
2020-06-20 23:40:21 +0700

    Test Xoa Sách

--- TestQLSach/XoaS.cs
+++ TestQLSach/XoaS.cs
@@ -6,9 +6,37 @@ namespace TestQLSach
     [TestClass]
     public class XoaS
     {
-        [TestMethod]
-        public void TestMethod1()
+        private QuanLyThuVien.DAO.Sach_DAO sa;
+        private QuanLyThuVien.DTO.Sach_DTO s;
+        [TestInitialize]
+        public void CNXoa()
         {
+            this.sa = new QuanLyThuVien.DAO.Sach_DAO();
+        }
+
+        [TestMethod]
+        public void XoaTrongMS()
+        {
+            this.s = new QuanLyThuVien.DTO.Sach_DTO();
+            s.MaSach = "";
+
+            Assert.AreEqual(sa.Delete(s.MaSach), false);
+        }
+        [TestMethod]
+        public void XoaSaiMS()
+        {
+            this.s = new QuanLyThuVien.DTO.Sach_DTO();
+            s.MaSach = "1009";
+
+            Assert.AreEqual(sa.Delete(s.MaSach), false);
+        }
+        [TestMethod]
+        public void XoaSach()
+        {
+            this.s = new QuanLyThuVien.DTO.Sach_DTO();
+            s.MaSach = "1007";
+
+            Assert.AreEqual(sa.Delete(s.MaSach), true);
         }
     }
 }
