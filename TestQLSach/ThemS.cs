93b429809cb467f06469b02eaaff0b087bc624d6
GLinh1210 <1654050058linh@ou.edu.vn>
2020-06-20 23:31:49 +0700

    TestSach

--- TestQLSach/ThemS.cs
+++ TestQLSach/ThemS.cs
@@ -24,18 +24,12 @@ namespace TestQLSach
             Assert.AreEqual(sa.Insert(s), false);
         }
         [TestMethod]
-        public void ThemDGDung()
+        public void TrungTenSach()
         {
-            this.acc = new QuanLyThuVien.DTO.Account_DTO();
-            this.DG = new QuanLyThuVien.DTO.DocGia_DTO();
-            acc.MaDocGia = "114";
-            acc.TenDangNhap = "TestDG";
-            DG.HoTen = "Test Độc Giả";
-            DG.NamSinh = DateTime.Today;
-            DG.GioiTinh = "Nữ";
-
-
-            Assert.AreEqual(dg.ThemDG(DG, acc), true);
+            this.s = new QuanLyThuVien.DTO.Sach_DTO();
+            s.MaSach = "1006";
+            s.TenSach = "Nhập môn tin học";
+            Assert.AreEqual(sa.Insert(s), false);
         }
     }
 }
