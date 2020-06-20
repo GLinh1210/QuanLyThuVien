802c4be18addb7511d3cf8c5ca91d37a8434754d
GLinh1210 <1654050058linh@ou.edu.vn>
2020-06-20 23:34:37 +0700

    Test Xoa Sách

---
+++ TestQLSach/XoaS.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestQLSach
+{
+    [TestClass]
+    public class XoaS
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
