b640535aa40da8aedb393069101999de9fa763f4
GLinh1210 <1654050058linh@ou.edu.vn>
2020-06-21 19:55:41 +0700

    Test TDTT

--- TestTDTT/ThayDoiTT.cs
+++ TestTDTT/ThayDoiTT.cs
@@ -8,7 +8,15 @@ namespace TestTDTT
     {
         private QuanLyThuVien.DAO.DocGia_DAO DG;
         private QuanLyThuVien.DTO.DocGia_DTO dg;
-        
+        private QuanLyThuVien.DAO.DoiMK_DAO mk;
+        private QuanLyThuVien.DTO.Account_DTO acc;
+
+        [TestInitialize]
+        public void CNDoiMK()
+        {
+
+            this.mk = new QuanLyThuVien.DAO.DoiMK_DAO();
+        }
         [TestInitialize]
         public void CNTDTT()
         {
@@ -21,7 +29,6 @@ namespace TestTDTT
         {
             string tdn = "TestThayDoi";
             this.dg = new QuanLyThuVien.DTO.DocGia_DTO();
-            //dg.TenDangNhap = "TestThayDoi";
             dg.HoTen = "Thay Đổi";
             dg.GioiTinh = "Nữ";
             dg.NamSinh = DateTime.Today;
