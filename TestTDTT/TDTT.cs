3683792565de49723e8bb976711092b85c43f4b8
GLinh1210 <1654050058linh@ou.edu.vn>
2020-06-21 20:15:14 +0700

    Test Thay Đổi Thông Tin Độc Giả

---
+++ TestTDTT/TDTT.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestTDTT
+{
+    [TestClass]
+    public class TDTT
+    {
+        private QuanLyThuVien.DAO.DocGia_DAO DG;
+        private QuanLyThuVien.DTO.DocGia_DTO dg;
+
+
+
+        [TestInitialize]
+        public void CNTDTT()
+        {
+
+            this.DG = new QuanLyThuVien.DAO.DocGia_DAO();
+        }
+
+        [TestMethod]
+        public void ThayDoiTTDG()
+        {
+            string tdn = "TestThayDoi";
+            this.dg = new QuanLyThuVien.DTO.DocGia_DTO();
+            dg.HoTen = "Thay Đổi";
+            dg.GioiTinh = "Nữ";
+            dg.NamSinh = DateTime.Today;
+            dg.DiaChi = "113 Nguyễn Kiệm";
+            Assert.AreEqual(DG.SuaDG(dg, tdn), true);
+
+        }
+    }
+}
